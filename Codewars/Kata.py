6cc334117d799f384c1071b1255f69a2328f9c35
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2024-04-02 19:10:36 +0100

    Update Kata.py

--- Codewars/Kata.py
+++ Codewars/Kata.py
@@ -9,4 +9,28 @@ def filter_list(l): # Can use try except to filter list
             
     return output
 
-def 
+def reverse_words(text): # Use of map and join
+    wordlist = text.split(" ")
+    x = list(map(reverse, wordlist))
+    return ' '.join(x)
+    
+def reverse(word): # How to reverse a string
+    return word[::-1]
+
+def add_binary(a,b): # Convert integer to binary
+    return bin(a+b)[2:] # [2:] removes left two chars from a string
+
+def digital_root(n):
+    nlist = [int(x) for x in str(n)]
+    if len(nlist) == 1:
+        return nlist[0]
+    else: # join only works on lists of strings
+        return nlist[0] + digital_root(int(''.join(map(str, nlist[1:]))))
+    # For the rest of the functionality, need to use the sum function on the list and then recursively check if the value is below 10
+    
+def find_nb(m):
+    count = 1
+    while m != 0:
+        m = m - count**3
+        count += 1
+    return count
